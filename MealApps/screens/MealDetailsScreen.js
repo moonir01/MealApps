d305ce73e6ae8f3fe15467913dfae08ee4922417
Md. Monirozzaman Khan <monirkhan37@outlook.com>
2021-07-16 02:03:55 +0600

    20210716 Update navigation and header icon

--- MealApps/screens/MealDetailsScreen.js
+++ MealApps/screens/MealDetailsScreen.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+
 import { MEAL, MEALS } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -25,7 +29,24 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectMeal.title,
-    headerRight: <Text>Fav</Text>
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite");
+          }}
+        />
+           <Item
+          title="Favorite"
+          iconName="ios-star-outline"
+          onPress={() => {
+            console.log("Mark as favorite");
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
 
