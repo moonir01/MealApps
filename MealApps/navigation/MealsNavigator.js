ec85c9f0aa4130cb328af3ec7fcb869076f7905d
Md. Monirozzaman Khan <monirkhan37@outlook.com>
2021-07-17 10:00:42 +0600

    20211707  Botom tab navigation icon setting

--- MealApps/navigation/MealsNavigator.js
+++ MealApps/navigation/MealsNavigator.js
@@ -1,52 +1,81 @@
-import { platform } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import React from "react";
+import { platform } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 //import { NavigationContainer } from '@react-navigation/native';
 //import { createStackNavigator } from '@react-navigation/stack';
 //import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import CategoriesScreen from '../screens/CategoriesScreen';
-import CategoryMealsScreen from '../screens/CategoryMealsScreen';
-import MealDetailScreen from '../screens/MealDetailsScreen';
-import FavoritesScreen  from '../screens/FiltersScreen';
-import Colors from '../constants/Colors';
-
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailsScreen";
+import FavoritesScreen from "../screens/FiltersScreen";
+import Colors from "../constants/Colors";
 
 const MealsNavigator = createStackNavigator(
-
   {
     Categories: {
       screen: CategoriesScreen,
       navigationOptions: {
-        headerTitle: 'Meal Categories !!!'
-      }
+        headerTitle: "Meal Categories !!!",
+      },
     },
     CategoryMeals: {
       screen: CategoryMealsScreen,
     },
-    MealDetail: MealDetailScreen
+    MealDetail: MealDetailScreen,
   },
   {
     //mode:'modal',
     //initialRouteName:'MealDetail',
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.PrimaryColor : ''
+        backgroundColor: Platform.OS === "android" ? Colors.PrimaryColor : "",
       },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.PrimaryColor,
-     // headerTitle: 'A Screen'// defual header title
-    }
-
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.PrimaryColor,
+      // headerTitle: 'A Screen'// defual header title
+    },
   }
-
 );
 
 //---------------Bottom TAB Navigator-------------------
 
-const MealFavTabNavigator = createBottomTabNavigator({
-  Meals:MealsNavigator ,//copy setup from MealsNavigator
-  Favorites: FavoritesScreen// Call from favorit screen
-});
+const MealFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabinfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={25}
+              color={tabinfo.tintColor}
+            />
+          );
+        },
+      },
+    }, //copy setup from MealsNavigator
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarLabel: "Favorites",
+        tabBarIcon: (tabinfo) => {
+          return (
+            <Ionicons name="ios-star" size={25} color={tabinfo.tintColor} />
+          );
+        },
+      },
+    }, // Call from favorit screen
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
 
-export default createAppContainer(MealFavTabNavigator);//MealsNavigator nested undwer  MealFavTabNavigator bcoz we can only use one root navigator in app container
+export default createAppContainer(MealFavTabNavigator); //MealsNavigator nested undwer  MealFavTabNavigator bcoz we can only use one root navigator in app container
